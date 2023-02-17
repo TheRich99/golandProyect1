595112768ab212686e420f8b5cb95d07f4b8e229
Brian lml <brian.arias@unillanos.edu.co>
2023-02-17 05:51:47 -0500

    ultimos cambios

--- server_frond/public/js/listadoPedidos.js
+++ server_frond/public/js/listadoPedidos.js
@@ -7,16 +7,10 @@ var platosP = document.getElementById("platosP");
 var valorP = document.getElementById("valorP");
 var mesaP = document.getElementById("mesaP");
 
-var p1 = document.getElementById("p1");
-var p2 = document.getElementById("p2");
 
 function detalles(a,b,c){
     fechaP.value =  new Date(); 
     platosP.value = b;
     valorP.value = c;
     mesaP.value = a;
-}
-
-function completar(){
-    p1.style.display = "none";
 }
