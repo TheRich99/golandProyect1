4d076e7654cbed994bb9628f36ec45a90fc5fdae
Brian lml <brian.arias@unillanos.edu.co>
2023-02-17 03:53:44 -0500

    Menu y resumen pedido (sin guardar en DB)

--- server_frond/public/js/main.js
+++ server_frond/public/js/main.js
@@ -1,111 +1,205 @@
 //var $ = jQuery;
 var $ = jQuery.noConflict();
-$(document).ready(function(){
-    
+var inventarioPedido;
+
+var contador = 0;
+let arregloPedido = [];
+var masBoton = document.getElementById("masbutton");
+var menosBoton = document.getElementById("menosbutton");
+
+
+
+
+function mas(boton) {
+    //precio sin $
+    let precioC;
+    //div contenedor 
+    var div = boton.closest(".card");
+    // Nombre plato
+    var h5 = div.querySelector(".card-title");
+    var nombre = h5.textContent;
+    // contador plato
+    var div2 = div.querySelector(".contador");
+    var contador2 = div2.textContent;
+    // id plato
+    var div3 = div.querySelector("#idP");
+    var id = div3.textContent;
+    // precio plato
+    var div4 = div.querySelector("#precioP");
+    var precio = div4.textContent.substring(1);
+
+    var existe = false;
+    var position;
+    contador = contador2;
+    contador++;
+
+    // Si el elemento existe, actualiza su cantidad
+    if (contador > 0) {
+        for (let i = 0; i < arregloPedido.length; i++) {
+            if (arregloPedido[i][0] === nombre) {
+                position = i;
+                existe = true;
+                //console.log("existe," + nombre);
+                break;
+            }
+        }
+        if (existe) {
+            arregloPedido[position][1] = contador;
+            existe = false;
+            //console.log("contador+:" + contador + "," + arregloPedido[position][0]);
+        } else {
+            //console.log("nuevo");
+            arregloPedido.push([nombre, 1, id, precio]);
+            //console.log("arreglo+" + arregloPedido)
+        }
+    }
+    div2.innerHTML = contador;
+}
+
+function menos(boton) {
+    var div = boton.closest(".card");
+    var h5 = div.querySelector(".card-title");
+    var nombre = h5.textContent;
+    var div2 = div.querySelector(".contador");
+    var contador2 = div2.textContent;
+
+    contador = contador2;
+
+    if (contador > 0) {
+        contador--;
+        //console.log("contador-:" + contador + "," + nombre);
+    }
+    if (contador == 0) {
+        for (let i = 0; i < arregloPedido.length; i++) {
+            if (arregloPedido[i][0] === nombre) {
+                arregloPedido.splice(i, 1);
+                break;
+            }
+        }
+    }
+    div2.innerHTML = contador;
+}
+
+function crearPedido() {
+    if (arregloPedido.length === 0) {
+        window.alert("asegurate de seleccionar al menos un plato");
+    } else {
+        var queryString = "?arregloPedido=" + encodeURIComponent(JSON.stringify(arregloPedido));
+        var url = "Pedido.html" + queryString;
+        window.location.href = url;
+    }
+}
+
+$(document).ready(function () {
+
     listado();
 });
+function contador() {
 
-function listado(){
+}
+
+function listado() {
     $.ajax({
-        url:"http://localhost:3000/platos/",
-        type:"get",
-        dataType:"json",
-        success:function(response){
+        url: "http://localhost:3000/platos/",
+        type: "get",
+        dataType: "json",
+        success: function (response) {
             //console.log(response);
-            
-            const data =  response
+
+            const data = response
             console.log(data.Message);
             console.log(data.Platos);
-            if(data.Message == "Success"){
+            if (data.Message == "Success") {
 
-                const contenido=document.getElementById("contenido")
-                
-                contenido.innerHTML="";
-                var contador =0;
+                const contenido = document.getElementById("contenido")
+
+                contenido.innerHTML = "";
+                var contador = 0;
                 data.Platos.forEach(p => {
-                    
-                    contenido.innerHTML+=`      <section class="col">
+
+                    contenido.innerHTML += `      <section class="col">
                             <div class="card">
-                            <img img id="imgMenu" src="/public/Imagenes/Menu/Churrasco.jpg" class="card-img-top">
-                            <div class="card-body">
-                                <h5 class="card-title">${p.nombre}</h5>
-                                <p class="card-text">${p.descripcion}</p>
-                                <h5 class="card-text lead"><b>$${p.precio}</b></h5>
-                            </div>
-                            <div class="card-footer">
-                                <div class="d-grid gap-2 d-md-block">
-                                <button type="button" class="btn btn-success btn-sm"><i class="fas fa-shopping-bag fa-fw"></i>
-                                    Agregar</button>
-                                <button type="button" class="btn btn-danger btn-sm"><i class="far fa-trash-alt"></i> Eliminar</button>
+                                <img img id="imgMenu" src="/public/Imagenes/Menu/Churrasco.jpg" class="card-img-top">
+                                <div class="card-body">
+                                    <p class="card-text" id="idP" style="display:none">${p.ID}</p>
+                                    <h5 class="card-title" id="nombre">${p.nombre}</h5>
+                                    <p class="card-text">${p.descripcion}</p>
+                                    <h5 class="card-text lead" id="precioP"><b>$${p.precio}</b></h5>
+                                </div>
+                                <div class="card-footer ">
+                                    <div class="d-grid gap-2 d-md-block align-items-end">
+                                    <button id="masbutton" onclick="mas(this)" type="button" class="btn btn-success btn-sm"><i class="fa-solid fa-plus">+</i></button>
+                                    <button id="menosbutton" onclick="menos(this)" type="button" class=" btn btn-danger btn-sm"> <i class="fa-solid fa-minus">-</i></button>
+                                    <div class="contador" id="contador">0</div>
+                                    </div>
                                 </div>
                             </div>
-                            </div>
                         </section>`
-                    
+
                 });
 
-                
-                
-            }else{
+
+
+            } else {
                 alert("platos no encontrados")
             }
         },
-        error: function(error){
+        error: function (error) {
             console.log(error);
         }
     });
-} 
+}
 
-function abrir_creacion(url){
-    $('#crear_plato').load(url,function(){
+function abrir_creacion(url) {
+    $('#crear_plato').load(url, function () {
         $(this).modal('show');
     });
 }
 
-function cerrar_creacion(){
+function cerrar_creacion() {
     $('#crear_plato').modal('hide');
 }
 
-function crear_p(){
+function crear_p() {
 
-/*     alert("hola");
-    let nombre = document.getElementById("campoNombre")
-    let descripcion = document.getElementById("exampleFormControlTextarea1")
-    let precio = document.getElementById("precio")
-    let cantidad = document.getElementById("cantidad")
-    let imagen = document.getElementById("imagen")
+    /*     alert("hola");
+        let nombre = document.getElementById("campoNombre")
+        let descripcion = document.getElementById("exampleFormControlTextarea1")
+        let precio = document.getElementById("precio")
+        let cantidad = document.getElementById("cantidad")
+        let imagen = document.getElementById("imagen")
+    
+        var formData = new FormData();
+        console.log(nombre.value);
+        formData.append('nombre',nombre.value);
+        formData.append('precio',precio.value);
+        formData.append('cantidad',cantidad.value);
+        formData.append('descripcion',descripcion.value);
+        formData.append('imagen',imagen.value); */
 
-    var formData = new FormData();
-    console.log(nombre.value);
-    formData.append('nombre',nombre.value);
-    formData.append('precio',precio.value);
-    formData.append('cantidad',cantidad.value);
-    formData.append('descripcion',descripcion.value);
-    formData.append('imagen',imagen.value); */
-  
-  
+
     const form = document.getElementById('form_crear_p');
     const formData = new FormData(form);
     console.log(form);
     $.ajax({
-        
+
         url: $('#form_crear_p').attr('action'),
         type: $('#form_crear_p').attr('method'),
         data: formData,
-        cache:false,
-        processData:false,
-        contentType:false,
-        success: function(response){
+        cache: false,
+        processData: false,
+        contentType: false,
+        success: function (response) {
             console.log(response);
-           
+
             alert('Entidad registrado correctamente');
             listado();
             cerrar_creacion();
 
         },
-        error:function(error){
+        error: function (error) {
             console.log(error);
-            
+
         }
 
     });
--- server_frond/public/js/metodos.js
+++ server_frond/public/js/metodos.js
@@ -1,11 +1,70 @@
-$(document).ready(function() {
-    $("#mostrarContra").click(function() {
-      var input = $("#password");
-      if (input.attr("type") === "password") {
-        input.attr("type", "text");
-      } else {
-        input.attr("type", "password");
-      }
-    });
+$(document).ready(function () {
+  $("#mostrarContra").click(function () {
+    var input = $("#password");
+    if (input.attr("type") === "password") {
+      input.attr("type", "text");
+    } else {
+      input.attr("type", "password");
+    }
   });
+});
+
+var queryString = window.location.search;
+var urlParams = new URLSearchParams(queryString);
+var arregloString = urlParams.get("arregloPedido");
+let arreglo = JSON.parse(decodeURIComponent(arregloString));
+
+var listaPedido = document.getElementById("listaProductos");
+var cantidadPlatos = document.getElementById("cantidadP");
+var totalP = document.getElementById("total");
+
+var total = parseInt(0);
+var cantidadP = 0;
+
+cargarPlatosPedido(arreglo);
+
+function cargarPlatosPedido(arregloPedido) {
+  for (i = 0; i < arregloPedido.length; i++) {
+    listaPedido.innerHTML += `<div class="container-fluid" id="${arregloPedido[i][2]}">
+                                                      <hr><br>
+                                                      <h5 class="poppins-regular font-weight-bold full-box text-center" >${arregloPedido[i][0]}</h5><br>
+                                                      <div class="bag-item full-box">
+                                                        <div class="full-box">
+                                                          <div class="container-fluid">
+                                                            <div class="row">
+                                                              <div class="col-12 col-lg-6 text-center mb-4">
+                                                                <div class="row justify-content-center">
+                                                                  <div class="col-auto">
+                                                                    <div class="form-outline mb-4">
+                                                                      <input type="number" value="${arregloPedido[i][1]}" class="form-control text-center" id="cantidad"
+                                                                        maxlength="10" disabled>
+                                                                      <label class="form-label">Cantidad</label>
+                                                                    </div>
+                                                                  </div>
+                                                                </div>
+                                                              </div>
+                                                              <div class="col-12 col-lg-4 text-center mb-4">
+                                                                <span class="poppins-regular font-weight-bold">SUBTOTAL: $${arregloPedido[i][3]}</span>
+                                                              </div>
+
+                                                            </div>
+                                                          </div>
+                                                        </div>
+                                                      </div>
+                                                      <hr>
+                                                    </div>`;
+    total += parseInt(arregloPedido[i][3]) * parseInt(arregloPedido[i][1]);
+    cantidadP += arregloPedido[i][1];
+  }
+  cantidadPlatos.innerHTML = cantidadP;
+  totalP.innerHTML = "$" + total;
+}
+
+
+function confirmarOrden() {
+  const fecha = new Date();
+  const select = document.getElementById("selectMesa");
+  const valorSeleccionado = select.value;
   
+  console.log(fecha, total, valorSeleccionado);
+}
